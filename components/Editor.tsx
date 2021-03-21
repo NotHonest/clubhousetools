e9cf2a55e88f8aa5e0f94859fe5fc28c48fc868d
Pavel Lubyanskiy <lubyanskiy@sports.ru>
2021-03-21 16:08:45 +0300

    Update Editor.tsx

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -52,8 +52,8 @@ const Editor = React.forwardRef(({style, text, src}: Props, ref: React.Forwarded
 						<>
 							<Rect
 								height={34}
-								width={190}
-								y={280}
+								width={380}
+								y={180}
 								x={(WIDTH / 2) - 95}
 								fill="#25AE60"
 								shadowColor="black"
